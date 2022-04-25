7610405608e7e4d7ad2f5d5b1d97b23956fc1b6c
MariaClaudia1982 <mariaclaudiapio@gmail.com>
2022-04-25 20:37:24 -0300

    terceira aula

--- index.js
+++ index.js
@@ -1,27 +1,5 @@
-class Cliente {
-    nome;
-    cpf;
-}
-
-class ContaCorrente {
-    agencia;
-    // #saldo = 0; (ainda é só uma proposta)
-    _saldo = 0;
-
-    sacar(valor) {
-        if(this._saldo >= valor) {
-            this._saldo -= valor;
-            return valor; // o return para a execução do método
-        }else {
-            console.log("Você não tem saldo suficiente para essa transação.")
-        }
-    }
-
-    depositar(valor) {
-        if(valor <= 0) return;
-        this._saldo += valor; 
-    }
-}
+import {Cliente} from "./Cliente.js"
+import {ContaCorrente} from "./ContaCorrente.js" 
 
 const cliente1 = new Cliente();
 cliente1.nome = "Ricardo";
@@ -33,10 +11,15 @@ cliente2.cpf = 12345678901;
 
 const contaCorrenteRicardo = new ContaCorrente();
 contaCorrenteRicardo.agencia = 1001;
+contaCorrenteRicardo.cliente = cliente1;
+contaCorrenteRicardo.depositar(500);
 
-contaCorrenteRicardo.depositar(300);
-const valorSacado = contaCorrenteRicardo.sacar(50);
-console.log(valorSacado);
+const conta2 = new ContaCorrente();
+conta2.cliente = cliente2;
+conta2.agencia = 102;
 
-console.log(contaCorrenteRicardo);
+contaCorrenteRicardo.transferir(200, conta2);
+
+
+console.log(conta2);
 
